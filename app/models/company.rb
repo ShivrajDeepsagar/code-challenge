ae010b9a8c97f0fef2ca74c601b1d7b8cb449620
shivaraj <shivaraj.deepasagar@gmail.com>
2020-07-22 23:37:42 +0530

    email domain and zip code validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,12 @@
 class Company < ApplicationRecord
   has_rich_text :description
-
+  validates_format_of :zip_code,
+                  with: /\A\d{5}-\d{4}|\A\d{5}\z/,
+                  message: "should be 12345 or 12345-1234",
+                  allow_blank: true
+  validates_format_of :email,
+                  with: /\b[A-Z0-9._%a-z\-]+@+getmainstreet\.com\z/,
+                  message: "must be a getmainstreet.com account",
+                  allow_blank: true
 
 end
