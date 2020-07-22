00326701010c80e696de8d6f0715ce2bcbc7747e
shivaraj <shivaraj.deepasagar@gmail.com>
2020-07-22 23:40:56 +0530

    tests added

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -21,15 +21,17 @@ class CompaniesControllerTest < ApplicationSystemTestCase
     assert_text @company.name
     assert_text @company.phone
     assert_text @company.email
-    assert_text "City, State"
+    assert_text @company.city_state_blob['state']
+    assert_text @company.city_state_blob['city']
+
   end
 
   test "Update" do
     visit edit_company_path(@company)
-
     within("form#edit_company_#{@company.id}") do
       fill_in("company_name", with: "Updated Test Company")
       fill_in("company_zip_code", with: "93009")
+      fill_in("company_email", with: "new_test_company@getmainstreet.com")
       click_button "Update Company"
     end
 
@@ -42,12 +44,12 @@ class CompaniesControllerTest < ApplicationSystemTestCase
 
   test "Create" do
     visit new_company_path
-
     within("form#new_company") do
       fill_in("company_name", with: "New Test Company")
       fill_in("company_zip_code", with: "28173")
       fill_in("company_phone", with: "5553335555")
-      fill_in("company_email", with: "new_test_company@test.com")
+      fill_in("company_email", with: "new_test_company@getmainstreet.com")
+
       click_button "Create Company"
     end
 
@@ -56,6 +58,8 @@ class CompaniesControllerTest < ApplicationSystemTestCase
     last_company = Company.last
     assert_equal "New Test Company", last_company.name
     assert_equal "28173", last_company.zip_code
+    assert_match(/\b[A-Z0-9._%a-z\-]+@+getmainstreet\.com\z/, last_company.email)
+
   end
 
 end
